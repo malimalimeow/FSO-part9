2d058f79ab0d72c06788c54251bcd58a8517a037
Heidy <heidy1443@hotmail.com>
2026-07-26 23:48:54 +0100

    initialize course ,database and type 9.17 done

---
+++ course/src/App.tsx
@@ -0,0 +1,138 @@
+interface HeaderProps {
+  courseName: string;
+}
+
+interface ContentProps {
+  courseParts: CoursePart[];
+}
+
+interface PartProps {
+  part: CoursePart;
+}
+
+interface CoursePartBase {
+  name: string;
+  exerciseCount: number;
+  description?: string;
+}
+
+interface CoursePartBasic extends CoursePartBase {
+  kind: "basic";
+}
+
+interface CoursePartGroup extends CoursePartBase {
+  groupProjectCount: number;
+  kind: "group";
+}
+
+interface CoursePartBackground extends CoursePartBase {
+  backgroundMaterial: string;
+  kind: "background";
+}
+
+type CoursePart = CoursePartBasic | CoursePartGroup | CoursePartBackground;
+
+const Header = ({ courseName }: HeaderProps) => <h1>{courseName}</h1>;
+
+const Content = ({ courseParts }: ContentProps) =>
+  courseParts.map((part) => <Part key={part.name} part={part} />);
+
+const Part = ({ part }: PartProps) => {
+  const assertNever = (value: never): never => {
+    throw new Error(
+      `Unhandled discriminated union member: ${JSON.stringify(value)}`,
+    );
+  };
+
+  switch (part.kind) {
+    case "basic":
+      return (
+        <div>
+          <strong>
+            {part.name} {part.exerciseCount}
+          </strong>
+          <p>{part.description}</p>{" "}
+        </div>
+      );
+      break;
+    case "group":
+      return (
+        <div>
+          <strong>
+            {part.name} {part.exerciseCount}
+          </strong>
+          <p>project counts {part.groupProjectCount} </p>
+        </div>
+      );
+      break;
+    case "background":
+      return (
+        <div>
+          <strong>
+            {part.name} {part.exerciseCount}
+          </strong>
+          <p>{part.description}</p>
+        </div>
+      );
+      break;
+    default:
+      return assertNever(part);
+  }
+};
+
+const Total = ({ courseParts }: ContentProps) => {
+  const totalExercises = courseParts.reduce(
+    (sum, part) => sum + part.exerciseCount,
+    0,
+  );
+
+  return <p>Number of exercises {totalExercises}</p>;
+};
+
+const App = () => {
+  const courseName = "Half Stack application development";
+  const courseParts: CoursePart[] = [
+    {
+      name: "Fundamentals",
+      exerciseCount: 10,
+      description: "This is an awesome course part",
+      kind: "basic",
+    },
+    {
+      name: "Using props to pass data",
+      exerciseCount: 7,
+      groupProjectCount: 3,
+      kind: "group",
+    },
+    {
+      name: "Basics of type Narrowing",
+      exerciseCount: 7,
+      description: "How to go from unknown to string",
+      kind: "basic",
+    },
+    {
+      name: "Deeper type usage",
+      exerciseCount: 14,
+      description: "Confusing description",
+      backgroundMaterial:
+        "https://type-level-typescript.com/template-literal-types",
+      kind: "background",
+    },
+    {
+      name: "TypeScript in frontend",
+      exerciseCount: 10,
+      description: "a hard part",
+      kind: "basic",
+    },
+  ];
+
+  return (
+    <div>
+      <Header courseName={courseName} />
+      <Content courseParts={courseParts} />
+      <Total courseParts={courseParts} />
+    </div>
+  );
+};
+
+export default App;
